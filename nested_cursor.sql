228f5763c7de5f348d3211b881214df3bc7d3937
Miguel Leyva <leyvaguadalupemiguel@gmail.com>
2017-11-12 20:38:27 -0500

    add nested cursor

---
+++ nested_cursor.sql
@@ -0,0 +1,11 @@
+DECLARE
+  CURSOR c_codes IS SELECT DISTINCT job_id FROM employees;
+  CURSOR c_employees (code VARCHAR2) IS SELECT * FROM employees WHERE job_id = code;
+BEGIN
+  FOR code IN c_codes LOOP
+    DBMS_OUTPUT.PUT_LINE('-----' || code.job_id || '-----');
+    FOR employee IN c_employees(code.job_id) LOOP
+      DBMS_OUTPUT.PUT_LINE('Employee= ' || employee.first_name);
+    END LOOP;
+  END LOOP;
+END;
